f0202fa4f488a931d7e7235bc86b6be09a94a39b
eragongaming <eragongaming1@gmail.com>
2020-08-02 02:13:52 -0400

    V2: Has ban capability now

--- Tao.py
+++ Tao.py
@@ -2,6 +2,8 @@ import os
 import random
 import discord
 from discord.ext import commands
+import shelve
+
 
 # Assigning id's and bot object
 TOKEN = os.environ['TOKEN']
@@ -22,6 +24,9 @@ control = control_file_r.read().splitlines()
 unhinged_file_r.close()
 hunt_file_r.close()
 control_file_r.close()
+
+# Data management for nat20 log
+nat20 = shelve.open('nat20.txt', flag='c', writeback=True)
 
 # Preparing variables to track tardiness
 # (initial is for startup, prepared for storing list of players)
@@ -45,7 +50,15 @@ def reset():
     initial[:] = [0]
 
 
-# bans a user with a reason
+# Posts message to log when bot ready
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+    # channel=bot.get_channel(566459563319099403)
+    # await channel.send('Tao has returned from training.')
+
+
+# Bans a user with a reason
 @bot.command(name='kill', help='Ban a user')
 @commands.has_permissions(administrator=True)
 async def ban(ctx, member: discord.User = None, reason=None):
@@ -60,12 +73,61 @@ async def ban(ctx, member: discord.User = None, reason=None):
     await ctx.channel.send(f"I have slain {member}.")
 
 
-# Posts message to log when bot ready
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
-    # channel=bot.get_channel(566459563319099403)
-    # await channel.send('Tao has returned from training.')
+# # Mutes a user with a reason
+# @bot.command(name='silence', help='mute a user')
+# @commands.has_permissions(administrator=True)
+# async def mute(ctx, member: discord.User = None, reason=None):
+#     if member is None or member == ctx.message.author:
+#         await ctx.channel.send("You cannot be silenced")
+#         return
+#     if reason is None:
+#         reason = "Trash cannot speak"
+#     message = f"I have silenced you for {reason}"
+#     role = discord.utils.get(member.guild.roles, name="Tina's Punishment")
+#     await member.send(message)
+#     await bot.add.roles(member, role, reason=reason)
+#     await ctx.channel.send(f"{member} is now unable to speak.")
+
+
+# Records natural 20's
+@bot.command(name='log20', help='''Records someone getting a nat20. Ex.(log20 @user). 
+May be used for corrections in format (log20 @user correction number''')
+@commands.has_role('Dungeoneer')
+async def nat20log(ctx, member: discord.User = None):
+    auth = str(ctx.message.author)
+    memb=str(member)
+    if 'correction' in ctx.message.content:
+        for character in ctx.message.content:
+            if character.isnumeric():
+                if member is None or member == ctx.message.author:
+                    nat20[auth]-=int(character)
+                    await ctx.send(f"The count for {ctx.message.author} is now {nat20[auth]}.")
+                    return
+                else:
+                    nat20[memb]-=int(character)
+                    await ctx.send(f"The count for {member} is now {nat20[memb]}.")
+                    return
+    if member is None or member == ctx.message.author:
+        if auth not in nat20:
+            nat20[auth]=0
+        nat20[auth]+=1
+        return
+    else:
+        if memb not in nat20:
+            nat20[memb]=0
+        nat20[memb]+=1
+
+
+# Shows current nat20 log
+@bot.command(name='show20', help="Shows log of nat20's. Ex.(show20)")
+@commands.has_role('Dungeoneer')
+async def nat20log(ctx):
+    await ctx.send("Here is a list of people and the amount of natural 20's they have got: ")
+    temp_log=[]
+    for key in nat20:
+        temp_log.append((key, nat20[key]))
+    for pair in temp_log:
+        await ctx.send(f"{pair[0]} has rolled {pair[1]} natural 20's.")
 
 
 # After called, starts tracking who speaks in discord
@@ -165,6 +227,8 @@ async def save(ctx):
         hunt_file_w.write(person_id+'\n')
     for person_id in control:
         control_file_w.write(person_id+'\n')
+    nat20.sync()
+    nat20.close()
     await ctx.send('Player data has been saved')
     await ctx.send('Tao is leaving to train.')
     await bot.logout()
